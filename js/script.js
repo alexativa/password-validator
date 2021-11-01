30989d3c09eac2a83636e9bdc4d09c4cf8a475e5
Aleksandra Tkaczyk <77849108+tkaale@users.noreply.github.com>
2021-11-01 12:39:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const minValue = 10;
 
 const checkPassword = () => {
     if (pass.value.length >= minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
-        p.innerHTML = 'Masz mocarne hasło!';
+        p.innerHTML = 'Masz mocne hasło!';
         p.style.color = "lime";
     } else if (pass.value.length >= minValue && pass.value.match(letters) && pass.value.match(numbers)) {
         p.innerHTML = 'Masz dobre hasło.';
